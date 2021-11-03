b3c6d8cd6a9253772fd81c3725ef76c283b25a14
yucong <windmillyucong@163.com>
2021-11-03 17:29:53 +0800

    impl Viewer

--- include/Viewer.h
+++ include/Viewer.h
@@ -27,72 +27,50 @@
 
 #include <mutex>
 
-namespace ORB_SLAM3
-{
+namespace ORB_SLAM3 {
 
 class Tracking;
 class FrameDrawer;
 class MapDrawer;
 class System;
 
-class Viewer
-{
-public:
-    Viewer(System* pSystem, FrameDrawer* pFrameDrawer, MapDrawer* pMapDrawer, Tracking *pTracking, const string &strSettingPath);
+class Viewer {
+ public:
+  Viewer(System *pSystem,
+         FrameDrawer *pFrameDrawer,
+         MapDrawer *pMapDrawer,
+         Tracking *pTracking,
+         const string &strSettingPath);
 
-    // Main thread function. Draw points, keyframes, the current camera pose and the last processed
-    // frame. Drawing is refreshed according to the camera fps. We use Pangolin.
-    void Run();
+  ~Viewer();
 
-    void RequestFinish();
+  // Main thread function. Draw points, keyframes, the current camera pose and the last processed
+  // frame. Drawing is refreshed according to the camera fps. We use Pangolin.
+  void Run();
 
-    void RequestStop();
+  void RequestFinish();
 
-    bool isFinished();
+  void RequestStop();
 
-    bool isStopped();
+  bool isFinished();
 
-    bool isStepByStep();
+  bool isStopped();
 
-    void Release();
+  bool isStepByStep();
 
-    void SetTrackingPause();
+  void Release();
 
-    bool both;
-private:
+  void SetTrackingPause();
 
-    bool ParseViewerParamFile(cv::FileStorage &fSettings);
-
-    bool Stop();
-
-    System* mpSystem;
-    FrameDrawer* mpFrameDrawer;
-    MapDrawer* mpMapDrawer;
-    Tracking* mpTracker;
-
-    // 1/fps in ms
-    double mT;
-    float mImageWidth, mImageHeight;
-
-    float mViewpointX, mViewpointY, mViewpointZ, mViewpointF;
-
-    bool CheckFinish();
-    void SetFinish();
-    bool mbFinishRequested;
-    bool mbFinished;
-    std::mutex mMutexFinish;
-
-    bool mbStopped;
-    bool mbStopRequested;
-    std::mutex mMutexStop;
-
-    bool mbStopTrack;
-
+  bool both;
+ private:
+  // PIMPL
+  class Impl;
+  std::shared_ptr<Impl> impl_;
 };
 
 }
 
-
 #endif // VIEWER_H
-	
+
 
