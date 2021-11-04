51d7a8ec538282aca32e982e5e73c5a4b8ad4de4
yucong <windmillyucong@163.com>
2021-11-04 15:19:44 +0800

    format ORBVocabulary

--- include/ORBVocabulary.h
+++ include/ORBVocabulary.h
@@ -23,11 +23,10 @@
 #include"Thirdparty/DBoW2/DBoW2/FORB.h"
 #include"Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h"
 
-namespace ORB_SLAM3
-{
+namespace ORB_SLAM3 {
 
 typedef DBoW2::TemplatedVocabulary<DBoW2::FORB::TDescriptor, DBoW2::FORB>
-  ORBVocabulary;
+    ORBVocabulary;
 
 } //namespace ORB_SLAM
 
