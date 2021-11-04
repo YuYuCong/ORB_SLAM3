8b27975695c061d926cbdf3a56c3b6565e2f47c7
yucong <windmillyucong@163.com>
2021-11-04 14:53:13 +0800

    format build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
-
 cd Thirdparty/DBoW2
 mkdir build
 cd build
@@ -7,24 +6,19 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 cd ../../g2o
-
 echo "Configuring and building Thirdparty/g2o ..."
-
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 cd ../../../
-
 echo "Uncompress vocabulary ..."
-
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
+
 cd ..
-
 echo "Configuring and building ORB_SLAM3 ..."
-
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
